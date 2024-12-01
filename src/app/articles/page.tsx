f3569216c05414fd8ebf73a5773412a00613a62f
Kenta Goto <kenta9736@gmail.com>
2024-12-01 10:22:32 +0900

    記事一覧のスタイルを整えた

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { getArticles } from "@/libs/client";
+import Image from "next/image";
 
 export default async function page() {
   const { contents } = await getArticles(100);
@@ -11,34 +12,73 @@ export default async function page() {
   return (
     <div className="bg-base text-gray-800 min-h-screen">
       {/* ページタイトル */}
-      <section className="p-10 text-gray-500 text-center">
+      <section className="px-20 py-10 text-gray-500 text-center">
         <div className="w-[60%] min-w-[525px] mx-auto">
           <h2 className="text-3xl font-bold">記事一覧</h2>
         </div>
       </section>
 
       {/* 記事リスト */}
-      <section className="w-[70%] min-w-[600px] max-w-[1000px] mx-auto m-10">
-        <div className="">
-          {/* 記事カード */}
-          {contents?.map((article) => (
-            <div
-              key={article.id}
-              className="bg-cardGray p-5 m-7 rounded-lg shadow-lg hover:shadow-lg transition"
-            >
-              <h3 className="text-xl font-semibold text-accentBlue">
-                {article.title}
-              </h3>
-              <p className="mt-2 text-gray-600">{article.description}</p>
+      <section className="w-[90%] min-w-[700px] max-w-[850px] mx-auto px-28 m-10">
+        {/* カード間のマージンを設定 */}
+        {contents?.map((article, i) => (
+          <div
+            key={i}
+            className="bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl transition-all mb-12"
+          >
+            <div className="flex">
+              {article.thumbnail?.url && (
+                <Image
+                  src={article.thumbnail?.url}
+                  height={100}
+                  width={100}
+                  alt={"thumbnail"}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    objectFit: "cover",
+                    borderRadius: "7px",
+                    marginLeft: "10px",
+                  }}
+                />
+              )}
+              <div className="ml-5">
+                {/* 記事タイトル */}
+                <h3 className="ml-3 text-xl font-semibold text-blue-600 cursor-default">
+                  {article.title}
+                </h3>
+
+                {/* 記事説明 */}
+                <p className="ml-3 mt-2 text-gray-700 cursor-default">
+                  {article.description}
+                </p>
+              </div>
+            </div>
+            <div>
+              {/* タグ */}
+              <div className="mt-5 flex space-x-2">
+                {article.tags?.slice(0, 3).map((tag, index) => (
+                  <span
+                    key={index}
+                    className="bg-blue-100 text-blue-600 text-xs font-medium py-1 px-3 rounded-full"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+
+            {/* ボタン */}
+            <div className="flex justify-center max-w-[600px]">
               <Link
                 href={`/articles/${article.id}`}
-                className="mt-3 inline-block text-gray-600 hover:underline"
+                className="w-full text-center mt-4 bg-blue-500 text-white text-sm font-medium py-2 px-4 rounded-full hover:bg-blue-600 transition-colors"
               >
                 続きを読む
               </Link>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </section>
     </div>
   );
